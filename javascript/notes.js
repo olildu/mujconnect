dab59f7ab61c284495901753d956dd42017e5f72
olildu <ebinsanthosh06@gmail.com>
2023-06-11 17:09:02 +0400

    New

--- javascript/notes.js
+++ javascript/notes.js
@@ -1,3 +1,26 @@
-function dofunc(){
-    document.getElementById("nav").classList.toggle("change")
-}
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
+import { getAuth, onAuthStateChanged  } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCFde5Hdt3CTbVw71uK89JThLPCq-6iNa8",
+  authDomain: "muj-connect-d2e2b.firebaseapp.com",
+  projectId: "muj-connect-d2e2b",
+  storageBucket: "muj-connect-d2e2b.appspot.com",
+  messagingSenderId: "790443205869",
+  appId: "1:790443205869:web:020a252e60cc19ed3bd8e4",
+  databaseURL: "https://muj-connect-d2e2b-default-rtdb.asia-southeast1.firebasedatabase.app",
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth();
+
+document.getElementById("clicker").addEventListener("click", function() {
+    var note = document.getElementById("take-notes").value
+    const uid = auth.currentUser.uid;
+
+    set(ref(database, '/'+uid), {
+      notes: note
+      });
+})
